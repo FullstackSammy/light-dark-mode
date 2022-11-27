be2167348d41041633783d834064976902eb9af5
samzzii <nasstromsamuel@icloud.com>
2022-11-27 16:32:46 +0100

    Added localstorage for saved mode

--- script.js
+++ script.js
@@ -6,19 +6,22 @@ const image2= document.getElementById('image2');
 const image3 = document.getElementById('image3');
 const textBox = document.getElementById('text-box');
 
+// Dark or Light Images
+function imageMode(color) {
+    image1.src = `img/undraw_firmware_${color}.svg`;
+    image2.src = `img/undraw_hacker_mind_${color}.svg`;
+    image3.src = `img/undraw_web_development_${color}.svg`;
+}
+
+
 // Dark Mode Styles
 function darkMode() {
     nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
     textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
 
     toggleIcon.children[0].textContent = 'Dark Mode';
-    toggleIcon.children[1].classList.remove('fa-sun');
-    toggleIcon.children[1].classList.add('fa-moon');
-
-    image1.src = 'img/undraw_firmware_dark.svg';
-    image2.src = 'img/undraw_hacker_mind_dark.svg';
-    image3.src = 'img/undraw_web_development_dark.svg';
-
+    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+    imageMode('dark');
 }
 
 // Light Mode Styles
@@ -27,23 +30,34 @@ function lightMode() {
     textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';;
 
     toggleIcon.children[0].textContent = 'Light Mode';
-    toggleIcon.children[1].classList.remove('fa-moon');
-    toggleIcon.children[1].classList.add('fa-sun');
-
-    image1.src = 'img/undraw_firmware_light.svg';
-    image2.src = 'img/undraw_hacker_mind_light.svg';
-    image3.src = 'img/undraw_web_development_light.svg';
+    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+    imageMode('light');
 }
 
 // Switch Theme Dynamically
 function switchTheme(event) {
     if (event.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
         darkMode();
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
-        lightMode()
+        localStorage.setItem('theme', 'light');
+        lightMode();
     }
 }
 
 toggleSwitch.addEventListener('change', switchTheme);
+
+// Check Local Storage For Theme
+function storedTheme() {
+    const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        darkMode();
+    }
+}    
+}
