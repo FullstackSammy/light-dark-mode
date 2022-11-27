58b7eefdfbe3f56bcc9eaafb18911866a9ca485d
samzzii <nasstromsamuel@icloud.com>
2022-11-27 14:30:51 +0100

    Added eventlistener and constant for checkbox

---
+++ script.js
@@ -0,0 +1,8 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+
+// Switch Theme Dynamically
+function switchTheme(event) {
+    console.log(event.target.checked);
+}
+
+toggleSwitch.addEventListener('change', switchTheme);
